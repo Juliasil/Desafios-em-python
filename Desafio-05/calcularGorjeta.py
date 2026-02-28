ae0326b3b24e81de8ef8d5f0ab2dc61579250fd1
Juliana da Silveira Aguilar <julianasilveiraaguilar@gmail.com>
2026-02-28 11:36:28 -0300

    feat(d05): adiciona app de cálculo de gorjeta com divisão por pessoas

---
+++ Desafio-05/calcularGorjeta.py
@@ -0,0 +1,14 @@
+print('Seja bem-vindo(a) a APP Gorgeta!')
+
+account = float(input('Imforme o valor da conta: R$ '))
+
+percentage = int(input('Que porcentagem de gorjeta você gostaria de dar? 10%, 12% ou 15%? '))
+
+people = int(input('Quantas pessoas vão pagar a conta? '))
+
+accountWithTip = percentage / 100 * account + account
+
+totalvalue = accountWithTip /people
+
+print(f'Cada pessoa deve pagar: R$ {totalvalue:.2f}')
+
