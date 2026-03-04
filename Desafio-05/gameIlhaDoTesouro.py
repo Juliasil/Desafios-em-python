f563125b53e18695519b55cf755317323fe5963b
Juliana da Silveira Aguilar <julianasilveiraaguilar@gmail.com>
2026-03-04 11:07:36 -0300

    feat: implementa jogo Ilha do Tesouro com lógica condicional

---
+++ Desafio-05/gameIlhaDoTesouro.py
@@ -0,0 +1,47 @@
+print('Bem-vindo(a) à Ilha do Tesouro.')
+print('Sua missão é encontrar o tesouro.')
+
+print('''
+            ~     ~       ~
+-------------------------------------------------
+|                                                 |
+|      🧭                                         |
+|                                                 |
+|                 .-"""""""-.                     ||               .'            '.                   |
+|              /      🌴   🌴     \                  |
+|             ;                      ;              |
+|             |                      |              |
+|             |        ..........    |              |
+|             |        .        .    |              |
+|             |        .        .    |              |
+|             |        .        .    |              |
+|              \       .        .   /               |
+|               '.     .   💰   .  .'                |
+|                 '-.  .  _______  .-'               |
+|                   '. . /       \ .                 |
+|                    '. |  _______  ||               |
+|                      \| |  _   _  | |              |
+|                       | | | | | | | |              |
+|                       | | |_| |_| | |              |
+|                       |  \_________/  |            |
+|                        \______________/             |
+|                                                 |
+-------------------------------------------------
+
+~        ~        ~''')
+
+choice1 = input('''Você está em uma encruzilhada, para onde você deseja ir? Digite "esquerda" ou "direita".''').lower()
+if choice1 == 'esquerda':    
+    choice2 = input('você deseja nadar ou esperar?').lower()
+    if choice2 == 'nadar':
+        choice3 = input('Qual porta desja abrir: vermelha, Amarelo, Azul').lower()
+    if choice3 == 'vermelha':
+        print('Gamer Over')
+    elif choice3 == 'amarelo':
+        print('Parabéns você achou o tesouro')
+    elif choice3 == 'azul':
+        print('codigo por feras')
+    else:
+        print('Você escolheu uma alternativa que não existe')
+else:
+    print('Game Over')
